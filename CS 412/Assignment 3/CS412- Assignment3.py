4dcd59d34520790c1757ee79643ff64757d41897
Aman Arora <amanarora10@gmail.com>
2019-03-03 19:22:55 -0800

    Dataframe implementation

--- CS 412/Assignment 3/CS412- Assignment3.py
+++ CS 412/Assignment 3/CS412- Assignment3.py
@@ -1,7 +1,44 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
-data = np.loadtxt('places.txt',delimiter = ",")
+import random
+import sys
+import pandas as pd
+import pandas as pd
+def initalize_centroids(data, k):
+    centroids = []
+    while k>0:
+        n = random.randint(0,len(data))
+        centroid1 = data[n]
+        centroids.append(centroid1)
+        k = k-1
+    return centroids
+
+def assign_centroids(new_centroids, data, k):
+    data['dist0'] = math.hypot(data['x'] - new_centroids[0] , data['y']- new_centroid[0])
+    data['dist1'] = math.hypot(data['x'] - new_centroids[1] , data['y']- new_centroid[1])
+    data['dist2'] = math.hypot(data['x'] - new_centroids[2] , data['y']- new_centroid[2])
+   
+#def compute_new_centroids(data):
+    
+
+
+def run_k_means(new_centroids,data,k):
+    centroids = []
+    while (set(centroids) != set(new_centroids)):
+       assign_centroids(new_centroids,data,k)
+       centroids = newinit_centroids
+       new_centroids = compute_new_centroids(data)
+    return new_centroids 
+                            
+data= pd.read_csv(r'CS 412\Assignment 3\places.txt', sep=",", names = ["x", "y","dist0","dist1","dist2","cluster"])
+
+print(data.head())
+
 #print(data[:,0])
-plt.scatter(data[:,0], data[:,1])
-plt.show()
+#plt.scatter(data[:,0], data[:,1])
+#plt.show()
+k=3
+init_centroids = initalize_centroids(data,k )
+results = run_k_means(init_centroids, data, k) 
+
+
