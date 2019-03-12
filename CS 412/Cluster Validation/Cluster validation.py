b2db356c1d9591107b800b81da20e63cff0d028f
Arora, Aman <amana@qti.qualcomm.com>
2019-03-12 12:06:15 -0700

    Read data tested

--- CS 412/Cluster Validation/Cluster validation.py
+++ CS 412/Cluster Validation/Cluster validation.py
@@ -1,1 +1,22 @@
+import numpy
 
+def read_file(file_name, data): 
+    file = open(file_name,"r")
+    for line in file:
+      fields = line.split(" ")
+      index = int(fields[1])
+      data[index] = data[index] +1
+    return
+
+def read_data():
+    clusters =  numpy.zeros(shape=(5,5))
+    truth = numpy.zeros(5)
+    #Read ground truth
+    read_file('partitions.txt',truth)
+
+    #Read cluster data 
+    for i in range(0,5):
+        read_file('clustering_'+str(i+1) +'.txt',clusters[i])
+    return truth, clusters
+
+truth, clusters = read_data()
