2d7c76229214e190e85c81b9136f0a51594bfe29
Arora, Aman <amana@qti.qualcomm.com>
2019-03-12 17:57:36 -0700

    NMI complete

--- CS 412/Cluster Validation/Cluster validation.py
+++ CS 412/Cluster Validation/Cluster validation.py
@@ -1,5 +1,5 @@
 import numpy
-
+import math
 def read_file(file_name, data_count, data): 
     file = open(file_name,"r")
     i = 0
@@ -13,7 +13,7 @@ def read_file(file_name, data_count, data):
 
 def read_data():
     truth = numpy.zeros(300,dtype = int)
-    cluster= numpy.zeros([5,300],dtype = int)
+    clusters= numpy.zeros([5,300],dtype = int)
     cluster_count =  numpy.zeros(shape=(5,5),dtype = int)
     truth_count = numpy.zeros(5,dtype = int)
 
@@ -22,7 +22,58 @@ def read_data():
 
     #Read cluster data 
     for i in range(0,5):
-        read_file('clustering_'+str(i+1) +'.txt',cluster_count[i], cluster[i])
+        read_file('clustering_'+str(i+1) +'.txt',cluster_count[i], clusters[i])
     return truth,truth_count, clusters, cluster_count
 
-truth, truth_count,  cluster_count = read_data()
+def intersection(cluster, truth, cluster_id, truth_id):
+    nij = 0
+    i = 0
+    for entry in cluster:
+        if (entry == cluster_id and truth[i] == truth_id):
+           nij = nij + 1
+        i = i + 1
+    n =  len(cluster)
+    return (nij/n)
+
+
+def calculate_NMI(truth,truth_count, cluster_list, cluster_count ):
+    i = 0
+    # Calcuate Ict
+    Ict = 0
+    Hc = 0
+    Ht = 0
+    first_time = True
+    for cluster in cluster_count:
+        if(cluster == 0):
+            break
+        j = 0
+        Pci = cluster_count[i]/numpy.sum(cluster_count)                                                                                                                                                                                                                                                     
+        Hc  = Hc + Pci*math.log(Pci,2.0)
+        for partition in truth_count:
+            Pij = intersection(cluster_list, truth, i,j)
+            Ptj = truth_count[j]/numpy.sum(truth_count)
+            if(first_time == True and Ptj != 0 ):
+               Ht = Ht + Ptj*math.log(Ptj,2.0)                 
+            if(Pij !=0):
+                Ict = Ict + Pij* math.log(Pij/(Pci*Ptj), 2.0)
+                print('iteration:', i,j,'Pij:',Pij,Pij* math.log(Pij/(Pci*Ptj), 2.0) )
+            j = j+1
+        first_time = False   
+        i = i+1
+    NMI = Ict/(math.sqrt(Ht*Hc))
+    return NMI
+def calculate_Jaccard(truth,truth_count, cluster_list, cluster_count):
+
+    return 0
+
+result = []
+truth, truth_count, clusters, cluster_count = read_data()
+record = {'NMI':0, 'Jaccard':0}
+i = 0
+for cluster_list in clusters:
+    record['NMI'] = calculate_NMI(truth, truth_count, cluster_list, cluster_count[i] )
+    record['Jaccard'] = calculate_Jaccard(truth,truth_count, cluster_list, cluster_count)
+    result.append(record) 
+    i = i +1
+
+print(result)
---
+++ CS 412/Cluster Validation/Cluster-valid-sklearn.py
@@ -0,0 +1,4 @@
+from sklearn.metrics.cluster import normalized_mutual_info_score
+
+normalized_mutual_info_score([0, 0, 1, 1], [0, 0, 1, 1])
+
