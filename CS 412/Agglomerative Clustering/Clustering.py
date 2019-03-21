e355f1022155f22f0f417354d914894ca494fc30
Arora, Aman <amana@qti.qualcomm.com>
2019-03-20 17:18:01 -0700

    Distance matrix based - #19 failing

--- CS 412/Agglomerative Clustering/Clustering.py
+++ CS 412/Agglomerative Clustering/Clustering.py
@@ -3,39 +3,57 @@ import time
 #for line in sys.stdin:
     #print(line)
 result = [None]*2500
+
+
+def eucildian_distance(point1,point2):
+    dist = (point1[0]['x'] - point2[0]['x'])**2 + (point1[0]['y'] - point2[0]['y'])**2
+    return dist
+
+def compute_pairwise(loc_data,distance_matrix):
+    for i in range (0,len(loc_data)):
+        temp = []
+        for j in range (0, len(loc_data)):
+            dist = eucildian_distance(loc_data[i], loc_data[j])
+            distance_matrix[i][j] = dist
+
+
 def read_data():
     file = open("data.txt","r")
     first_line  = True
-    loc_data = [None]*2500
     i = 0
     for line in file:
       fields = line.split(" ")
       if first_line==True:                                               
         N, K, M  = [int(field) for field in fields]
+        loc_data = [None]*N
+        distance_matrix = [[0 for row in range(0,N)] for col in range(0,N)]
         first_line = False
       else:
           loc = [{'x': float(fields[0]) ,'y': float(fields[1]), 'index': i, 'cluster_id':i }]
           result[i] = i
           loc_data[i] = (loc)
           i = i+1
-
-    return N,K,M,loc_data
+    compute_pairwise(loc_data,distance_matrix)
+    return N,K,M,loc_data, distance_matrix
 
 def read_data_stdin():
     first_line  = True
-    loc_data = [None]*2500
     i = 0
     for line in sys.stdin:
       fields = line.split(" ")
       if first_line==True:                                               
         N, K, M  = [int(field) for field in fields]
+        loc_data = [None]*N
+        distance_matrix = [[0 for row in range(0,N)] for col in range(0,N)]
         first_line = False
       else:
           loc = [{'x': float(fields[0]) ,'y': float(fields[1]), 'index': i, 'cluster_id':i }]
           result[i] = i
           loc_data[i] = (loc)
           i = i+1
-    return N,K,M,loc_data
+    compute_pairwise(loc_data,distance_matrix)
+    return N,K,M,loc_data, distance_matrix
+    
 
 def merge_cluster(loc_data,i,j):
     for loc in loc_data[j]:
@@ -44,11 +62,7 @@ def merge_cluster(loc_data,i,j):
     del loc_data[j]
     return
 
-def eucildian_distance(point1,point2):
-    dist = (point1['x'] - point2['x'])**2 + (point1['y'] - point2['y'])**2
-    return dist
-
-def compare_cluster(loc_data, i,j, Measure):
+def compare_cluster(loc_data, i,j, Measure,distance_matrix):
 
     cluster1 = loc_data[i]
     cluster2 = loc_data[j]
@@ -57,13 +71,15 @@ def compare_cluster(loc_data, i,j, Measure):
         best['dist'] = sys.float_info.max
         for point1 in cluster1:
             for point2 in cluster2:
-                current = eucildian_distance(point1, point2)
+                #current = eucildian_distance(point1, point2)
+                current = distance_matrix[point1['index']][point2['index']]
                 if(current < best['dist']):
                     best = {'cluster1':i,'cluster2':j, 'dist': current}
     elif(Measure ==1):
         for point1 in cluster1:
             for point2 in cluster2:
-                current = eucildian_distance(point1, point2)
+                #current = eucildian_distance(point1, point2)
+                current = distance_matrix[point1['index']][point2['index']]
                 if(current > best['dist']):
                     best = {'cluster1':i,'cluster2':j, 'dist': current}
     elif(Measure ==2):
@@ -71,31 +87,28 @@ def compare_cluster(loc_data, i,j, Measure):
         agg_dist = 0
         for point1 in cluster1:
             for point2 in cluster2:
-                agg_dist = agg_dist + eucildian_distance(point1, point2)
+                agg_dist = agg_dist + distance_matrix[point1['index']][point2['index']]
                 count = count +1
        
         best = {'cluster1':i,'cluster2':j, 'dist': agg_dist/count}
 
     return best
 
-def check_clusters(loc_data, Measure, total_clusters):
-    start = time.perf_counter()
+def check_clusters(loc_data, Measure, total_clusters,distance_matrix):
     best = {'cluster1':0,'cluster2':0, 'dist': sys.float_info.max}
     for i in range(0, total_clusters -1 ):
         for j in range (i+1, total_clusters):
-            current = compare_cluster(loc_data,i,j, Measure)
+            current = compare_cluster(loc_data,i,j, Measure,distance_matrix)
             if(current['dist'] < best['dist']):
                best = current
     merge_cluster(loc_data,best['cluster1'],best['cluster2'])
-    end = time.perf_counter()
-    print("check_clusters()", end - start)
     return total_clusters -1
 
 def run_clustering():
-    N,K,M,loc_data = read_data()
+    N,K,M,loc_data, distance_matrix = read_data()
     cluster_count = N
     while cluster_count > K:
-        cluster_count = check_clusters(loc_data, M, cluster_count )
+        cluster_count = check_clusters(loc_data, M, cluster_count,distance_matrix )
     return loc_data, N
 
 loc_data, N = run_clustering()
@@ -103,7 +116,4 @@ loc_data, N = run_clustering()
 for line in result:
     if line is None:
         break
-    start = time.perf_counter()
     sys.stdout.write( str(line) + "\n")
-    end = time.perf_counter()
-    print("final print()", end - start)
--- CS 412/Agglomerative Clustering/clustering_optimized.py
+++ CS 412/Agglomerative Clustering/clustering_optimized.py
@@ -2,9 +2,8 @@ import sys
 import time
 #for line in sys.stdin:
     #print(line)
-result = [None]*2500
-unique = [None]*2500
-def read_data():
+
+def read_data(result):
     file = open("data.txt","r")
     first_line  = True
     loc_data = [None]*2500
@@ -13,6 +12,7 @@ def read_data():
       fields = line.split(" ")
       if first_line==True:                                               
         N, K, M  = [int(field) for field in fields]
+        result = [None]*N
         first_line = False
       else:
           loc = [{'x': float(fields[0]) ,'y': float(fields[1]), 'index': i, 'cluster_id':i }]
@@ -20,9 +20,9 @@ def read_data():
           loc_data[i] = (loc)
           i = i+1
 
-    return N,K,M,loc_data
+    return N,K,M,loc_data,result
 
-def read_data_stdin():
+def read_data_stdin(result):
     first_line  = True
     loc_data = [None]*2500
     i = 0
@@ -30,15 +30,16 @@ def read_data_stdin():
       fields = line.split(" ")
       if first_line==True:                                               
         N, K, M  = [int(field) for field in fields]
+        result = [None]*N
         first_line = False
       else:
           loc = [{'x': float(fields[0]) ,'y': float(fields[1]), 'index': i, 'cluster_id':i }]
           result[i] = i
           loc_data[i] = (loc)
           i = i+1
-    return N,K,M,loc_data
+    return N,K,M,loc_data, result
 
-def merge_cluster(loc_data,i,j):
+def merge_cluster(loc_data,i,j,result):
     for loc in loc_data[j]:
         result[loc['index']] = loc_data[i][0]['cluster_id']
     loc_data[i] = loc_data[i] + loc_data[j]
@@ -79,26 +80,18 @@ def compare_cluster(loc_data, i,j, Measure):
 
     return best
 
-def check_clusters(loc_data, Measure, total_clusters):
-    start = time.perf_counter()
+def check_clusters(loc_data, Measure, total_clusters,result):
     best = {'cluster1':0,'cluster2':0, 'dist': sys.float_info.max}
     for i in range(0, total_clusters -1 ):
         for j in range (i+1, total_clusters):
             current = compare_cluster(loc_data,i,j, Measure)
             if(current['dist'] < best['dist']):
                best = current
-    merge_cluster(loc_data,best['cluster1'],best['cluster2'])
-    end = time.perf_counter()
-    print("check_clusters()", end - start)
-    return total_clusters -1
+    merge_cluster(loc_data,best['cluster1'],best['cluster2'],result)
+    return total_clusters -1, result
 
-bool unique_cluster(cluster1, cluster2)
-{
-    if(unique[cluster1]! = None and  )
-    
-}
 
-def check_clusters_min(loc_data, Measure, total_clusters,K):
+def check_clusters_min(loc_data, Measure, total_clusters,K,result):
     dist_meas = []
 
     #Calculate distance measure
@@ -107,26 +100,38 @@ def check_clusters_min(loc_data, Measure, total_clusters,K):
             current = compare_cluster(loc_data,i,j, Measure)
             dist_meas.append(current)
     dist_meas.sort(key=lambda x: x['dist'])
-    for cand in dist_meas:
-        if(total_clusters != K):
-           result[cand['cluster2'] ] = result[cand['cluster1']]
-           if(unique_cluster(cand['cluster2'],cand['cluster1'] )):
-            total_clusters = total_clusters -1
-    return 
+    i = 0
+    for i in range(0,len(dist_meas1)-1):
+        if(total_clusters == K):
+            break
+        replace_cand = dist_meas[i]['cluster2']
+        new_cluster_id = dist_meas[i]['cluster1']
+        #update distance matrix
+        for j in range(i+1,len(dist_meas)):
+               if dist_meas[j]['cluster1'] ==replace_cand:
+                  dist_meas[j]['cluster1'] = new_cluster_id
+               elif dist_meas[j]['cluster2'] ==replace_cand:
+                  dist_meas[j]['cluster2'] = new_cluster_id
+        #update result
+        result = [new_cluster_id if x==replace_cand else x for x in result]
+        i = i+1
+        total_clusters = len(set(result))
+    return result
 
 
 def run_clustering():
-    N,K,M,loc_data = read_data()
+    result = []
+    N,K,M,loc_data,result = read_data(result)
     cluster_count = N
     if(M ==1 or M==2):
         while cluster_count > K:
-            cluster_count = check_clusters(loc_data, M, cluster_count )
+            cluster_count, result = check_clusters(loc_data, M, cluster_count,result )
     elif(M==0):
-            check_clusters_min(loc_data, M, cluster_count,K )
-    return loc_data, M
+            result = check_clusters_min(loc_data, M, cluster_count,K,result )
+    return loc_data, M, result
         
 
-loc_data, M = run_clustering()
+loc_data, M, result = run_clustering()
 
 for line in result:
     if line is None:
