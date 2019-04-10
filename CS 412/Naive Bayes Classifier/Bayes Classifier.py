01a61b5ec4371e5f7dbaef6026fd60ed0a7d8eb8
Aman Arora <amanarora10@gmail.com>
2019-04-10 16:42:19 -0700

    Code Complete

--- CS 412/Naive Bayes Classifier/Bayes Classifier.py
+++ CS 412/Naive Bayes Classifier/Bayes Classifier.py
@@ -1,4 +1,7 @@
 import sys
+from collections import Counter
+
+P_c = list()
 def read_data_stdin():
     X_train = list()
     y_train = list()
@@ -17,8 +20,22 @@ def read_data_stdin():
           X_test.append(list(map(int,row[1:17])))
     return X_train, y_train, X_test
 
+def ComputeLikeyhood(X_train,Y_train,test_row,c):
+    attribute_count = [0]*len(X_train[0])
+    likley_hood = 1
+    label_match_index = [i for i, y in enumerate(Y_train) if (y == c)] 
+    j = 0
+    for attribute in test_row:
+        for index in label_match_index:
+            if(X_train[index][j] == test_row[j]):
+                attribute_count[j] = attribute_count[j]  +1
+        attribute_count[j] = (attribute_count[j] + 0.1)/(len(label_match_index)+ 0.1*len(X_train[0]))                   
+        likley_hood = likley_hood*attribute_count[j] 
+        j = j+1
+    return likley_hood
+
 def read_data():
-    file = open(r"C:\Users\amana\source\repos\amanarora10\MCS-DS\CS 412\Naive Bayes Classifier\zoo.data","r")
+    file = open(r"C:\Users\amana\source\repos\amanarora10\MCS-DS\CS 412\Naive Bayes Classifier\test.data","r")
     X_train = list()
     y_train = list()
     X_test= list()
@@ -34,3 +51,27 @@ def read_data():
     return X_train, y_train, X_test
 
 X_train, y_train, X_test = read_data()
+
+P_c =  Counter(y_train)
+for element in P_c.keys():
+     P_c[element] = (P_c[element] + 0.1)/(len(y_train)+ 0.1*len(set(y_train)))
+
+result = [None] * len(X_test)
+i = 0
+for test_row in X_test: 
+    max = -1
+    for c in set(y_train):
+        P_x_c = ComputeLikeyhood(X_train,y_train,test_row,c)
+        current = P_c[c-1]*P_x_c
+        if(max<current):
+           result[i] = c  
+           max = current
+    i = i +1
+#text_file = open(r'C:\Users\amana\Source\Repos\amanarora10\MCS-DS\CS 412\Naive Bayes Classifier\result.txt', "w")
+#for label in result:
+#    print(label,file=text_file)
+
+for label in result:
+    if label is None:
+        break
+    sys.stdout.write( str(label) + "\n")
